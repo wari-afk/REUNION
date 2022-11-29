41c2f1d0164a56c30a6b2e49f3004fb895a3b8cb
Maria Andrello <maa.andrello@hotmail.com>
2022-11-29 17:35:06 +0100

    Added create method

--- app/controllers/reunions_controller.rb
+++ app/controllers/reunions_controller.rb
@@ -1,10 +1,17 @@
 class ReunionsController < ApplicationController
+
   def new
     @reunion = Reunion.new
   end
 
   def create
     @reunion = Reunion.new(reunion_params)
+    @reunion.user = current_user
+    if @reunion.save
+      redirect_to reunions_path
+    else
+      render :new
+    end
   end
 
   private

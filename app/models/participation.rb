f54b3ba1f6d3fb12ac5b3d56a9bea28814d80326
Pierre Warin <warin.pierre@gmail.com>
2022-11-29 17:42:25 +0100

    added controler

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,7 +2,4 @@ class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :reunion
   acts_as_voter
-  
-  validates :user_id, presence: true
-  validates :reunion_id, presence: true
 end

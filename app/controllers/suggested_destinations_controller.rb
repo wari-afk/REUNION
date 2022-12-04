220df8d5eb06ced8b62fbc3544f2e60c1d2e829a
joana costa <joanartdacosta@gmail.com>
2022-12-04 14:31:59 +0000

    randomdestination

--- app/controllers/suggested_destinations_controller.rb
+++ app/controllers/suggested_destinations_controller.rb
@@ -1,16 +1,15 @@
 class SuggestedDestinationsController < ApplicationController
-
   def index
     @suggested_destinations = SuggestedDestination.new
     @suggested_destinations = @links
   end
 
   def show
-  resources = Cloudinary::Api.resources(:type => :upload, :prefix => "reunion")["resources"].sample(3)
+    resources = Cloudinary::Api.resources(:type => :upload, :prefix => "reunion")["resources"].sample(3)
 
     @suggested_destinations = []
     resources.each do |resource|
-    @suggested_destinations << resource["secure_url"]
+      @suggested_destinations << resource["secure_url"]
     end
   end
 end

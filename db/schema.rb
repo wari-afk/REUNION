62333e0d1be503accd8aead163752fa11df09be0
Pierre Warin <warin.pierre@gmail.com>
2022-12-05 15:54:25 +0100

    new migration for schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_145823) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_142503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_145823) do
     t.bigint "reunion_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "has_voted_dates"
+    t.boolean "has_voted__on_dates"
     t.index ["reunion_id"], name: "index_participations_on_reunion_id"
     t.index ["user_id"], name: "index_participations_on_user_id"
   end

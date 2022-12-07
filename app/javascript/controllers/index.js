2279dd56a84c497f951e0f46f6363d156df86ec3
ajushnittarajiv <xajushnitta.411@gmail.com>
2022-12-07 18:11:52 +0100

    added animation when calling API

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,23 +2,25 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application";
+import { application } from "./application"
 
-import DateOptionsController from "./date_options_controller";
-application.register("date-options", DateOptionsController);
+import AnimationController from "./animation_controller"
+application.register("animation", AnimationController)
 
-import ParticipationsController from "./participations_controller";
-application.register("participations", ParticipationsController);
-
-import FlatpickrController from "./flatpickr_controller";
-application.register("flatpickr", FlatpickrController);
-
-import HelloController from "./hello_controller";
-application.register("hello", HelloController);
+import DateOptionsController from "./date_options_controller"
+application.register("date-options", DateOptionsController)
 
 import DisplayDestinationController from "./display_destination_controller"
 application.register("display-destination", DisplayDestinationController)
 
+import FlatpickrController from "./flatpickr_controller"
+application.register("flatpickr", FlatpickrController)
+
+import HelloController from "./hello_controller"
+application.register("hello", HelloController)
+
+import ParticipationsController from "./participations_controller"
+application.register("participations", ParticipationsController)
+
 import TomSelectController from "./tom_select_controller"
 application.register("tom-select", TomSelectController)
-
